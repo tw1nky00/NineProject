d466294ee0ce844bdfed4984ad5153f3185f0454
tw1nky00 <new.blank.list@gmail.com>
2024-01-30 23:25:04 +0300

    Added ingridient icons to the recipe UI blocks

--- Assets/Scripts/UIScripts/DeliveryManagerSingleUI.cs
+++ Assets/Scripts/UIScripts/DeliveryManagerSingleUI.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Responsible for single block in DeliveryManagerUI
@@ -10,6 +11,20 @@ public class DeliveryManagerSingleUI : MonoBehaviour
     /// The recipe name
     /// </summary>
     [SerializeField] private TextMeshProUGUI recipeNameText;
+    /// <summary>
+    /// An icon container
+    /// </summary>
+    [SerializeField] private GameObject iconContainer;
+    /// <summary>
+    /// An ingridient icon template
+    /// </summary>
+    [SerializeField] private GameObject ingridientTemplate;
+
+
+    private void Start()
+    {
+        this.ingridientTemplate.SetActive(false);
+    }
 
 
     /// <summary>
@@ -19,5 +34,25 @@ public class DeliveryManagerSingleUI : MonoBehaviour
     public void SetRecipeSO(RecipeSO recipe)
     {
         this.recipeNameText.text = recipe.RecipeName;
+
+        foreach (Transform icon in iconContainer.transform)
+        {
+            // Deleting all the icons
+
+            if (icon == this.ingridientTemplate) continue;
+            Destroy(icon.gameObject);
+        }
+
+
+        foreach (KitchenObjectSO ingridient in recipe.IngridientsList)
+        {
+            // Spawning all the ingridient icons
+            GameObject currentIngridientIconSpawned = Instantiate(this.ingridientTemplate, this.iconContainer.transform);
+            currentIngridientIconSpawned.SetActive(true);
+
+            // Setting an image
+            currentIngridientIconSpawned.GetComponent<Image>().sprite = ingridient.Sprite;
+            currentIngridientIconSpawned.GetComponent<Image>().enabled = true;
+        }
     }
 }
